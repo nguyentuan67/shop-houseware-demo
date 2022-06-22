df73427a3c91b3ebf06ae1799fb0f77caeee77a6
nguyentuan <dungtuann67@gmail.com>
2022-06-22 09:39:45 +0700

    upload first source code

---
+++ assets/js/main.js
@@ -0,0 +1,150 @@
+import products from "./products.js";
+
+const productList = document.querySelector('.product-list')
+const formSubmitWrapper = document.querySelector('.form-submit__wrapper')
+
+
+//render list all product
+function renderProduct() {
+    const productWraps = products.map(product => {
+        return `
+        <div class="product-item">
+            <div class="product-wrapper" data-id="${product.id}">
+                <div class="product-wrapper__img" style="background-image: url(${product.image});"></div>
+                <div class="product-wrapper__info">
+                    <h4 class="product-wrap__name">${product.name}</h4>
+                    <h5 class="product-wrap__desc">${product.description}</h5>
+                    <h5 class="product-wrap__price">${product.price} Đ</h5>
+                </div>
+                <div class="product__btn">
+                    Đặt hàng
+                </div>
+            </div>
+        </div>
+        `
+    })
+    productList.innerHTML = productWraps.join(' ')
+}
+renderProduct()
+
+
+//ẩn hiện form đăng kí
+let buyProductBtns = document.querySelectorAll('.product__btn')
+
+
+// thong tin san pham khi mua hang
+
+buyProductBtns.forEach(buyProductBtn => {
+    buyProductBtn.onclick = function(e) {
+        const idProductSelected = e.target.parentElement.dataset.id
+        const productSelected = products.find(product => {
+            return product.id == idProductSelected
+        })
+        renderFormSubmit(productSelected.name, productSelected.image)
+
+        const closedIcon = document.querySelector('.form-close--btn');
+        const overlay = document.querySelector('.overlay');
+        const formSubmit = document.querySelector('.form');
+
+        formSubmit.classList.add('open');
+        overlay.style.display = 'block';
+
+        //close formsubmit
+        closedIcon.onclick = function(){
+            formSubmit.classList.remove('open');
+            overlay.style.display = 'none';
+        }
+        
+        // click overlay to closed nav mobile
+        overlay.onclick = function(){
+            formSubmit.classList.remove('open');
+            overlay.style.display = 'none';
+        }
+
+        const formSubmitBtn = document.querySelector('.form-submit')
+        formSubmitBtn.onclick = function() {
+            let name = document.getElementById('fullname').value
+            let address = document.getElementById('address').value
+            let product = document.getElementById('name-product').innerText
+            let phone = document.getElementById('phone-number').value
+            let quantity = document.getElementById('quantity').value
+
+            if( name == "" ||  address == "" || phone == "" || quantity == "") {
+                alert("Vui lòng nhập đầy đủ thông tin")
+            } else {
+                let formOrder = {
+                    nameBuyer: name,
+                    address: address,
+                    phone: phone,
+                    product: product,
+                    quantity: quantity
+                }
+                createOrder(formOrder)
+                alert("Thành công! Đang chờ xác nhận.")
+                formSubmit.classList.remove('open');
+                overlay.style.display = 'none';
+            }
+    
+            
+        }
+
+    }
+})
+
+const orderApi = 'https://sheetdb.io/api/v1/nfgfzya2ffq4n'
+
+//create
+function createOrder(data, callback) {
+    var option = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    fetch(orderApi, option)
+        .then(respone => respone.json())
+        .then(callback)
+}
+
+
+
+function renderFormSubmit(name, img, price) {
+    const formHtml = `
+    <div class="form">
+        <div class="form-close--btn">
+            <i class="far fa-times"></i>
+        </div>
+        <div class="form-info">
+            <h2 class="content__desc text-center">Sản phẩm lựa chọn</h2>
+            <div class="product-wrapper__img" style="background-image: url(${img});"></div>
+            <h4 id="name-product" class="product-wrap__name text-center">${name}</h4>
+        </div>
+        <div class="form-wrapper">
+            <div class="form-group">
+                <label for="fullname" class="form-label">Họ và tên</label>
+                <input id="fullname" name="fullname" type="text" placeholder="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="phone-number" class="form-label">Số điện thoại</label>
+                <input id="phone-number" name="phone-number" type="text" placeholder="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="address" class="form-label">Địa chỉ</label>
+                <input id="address" name="address" type="text" placeholder="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="quantity" class="form-label">Số lượng</label>
+                <input id="quantity" name="quantity" type="text" value= "1" placeholder="" class="form-control">
+            </div>
+            <button class="form-submit">Đặt hàng</button>
+        </div>
+    </div>
+    `
+    formSubmitWrapper.innerHTML = formHtml
+}
+
+
+
+
+
---
+++ assets/js/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+        id: 1,
+        image: "./assets/image/gahongcherry.jpg",
+        description: "",
+        price: "199.000",
+        name: "Bộ ga màu hồng hình quả cherry"
+    },
+    {
+        id: 2,
+        image: "./assets/image/gahongdau.jpg",
+        description: "",
+        price: "199.000",
+        name: "Bộ ga màu hồng hình quả dâu"
+    },
+    {
+        id: 3,
+        image: "./assets/image/gahonggau.jpg",
+        description: "",
+        price: "199.000",
+        name: "Bộ ga màu hồng hình gấu"
+    },
+    {
+        id: 4,
+        image: "./assets/image/gaxanhbo.jpg",
+        description: "",
+        price: "199.000",
+        name: "Bộ ga màu xanh hình quả bơ"
+    },
+    {
+        id: 5,
+        image: "./assets/image/guong1.jpg",
+        description: "",
+        price: "350.000",
+        name: "Gương trang trí cơ lớn"
+    },
+    {
+        id: 6,
+        image: "./assets/image/binhnuoc1.jpg",
+        description: "",
+        price: "20.000",
+        name: "Bình đựng nước màu xanh"
+    },
+    {
+        id: 7,
+        image: "./assets/image/binhnuoc4.jpg",
+        description: "",
+        price: "139.000",
+        name: "Bình đựng nước màu trắng"
+    },
+    {
+        id: 8,
+        image: "./assets/image/binhnuoc3.jpg",
+        description: "",
+        price: "139.000",
+        name: "Bình đựng nước màu hồng"
+    },
+    {
+        id: 9,
+        image: "./assets/image/tulanh1.jpg",
+        description: "",
+        price: "299.000",
+        name: "Tủ lạnh mini màu vàng"
+    },
+    {
+        id: 10,
+        image: "./assets/image/tulanh3.jpg",
+        description: "",
+        price: "399.000",
+        name: "Tủ lạnh mini màu cam"
+    },
+    
+    
+]
+
+export default products
